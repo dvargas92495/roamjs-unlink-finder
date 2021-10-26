af7879eb2f88d3993c1817fd1bdfcab3398ee555
David Vargas <dvargas92495@gmail.com>
2021-10-26 17:48:40 -0400

    fix getallpages

--- unlink-finder/main.js
+++ unlink-finder/main.js
@@ -1,6 +1,7 @@
 function getAllPages() {
   return window.roamAlphaAPI
   .q("[:find ?t :where [?e :node/title ?t] ]")
+  .map(t => t[0])
   .sort(function (a, b) {
     return b.length - a.length;
   });
